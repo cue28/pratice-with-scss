79c277c4b6a23d91ff9a62684091f30beba974a5
siyoon <younyaho@gmail.com>
2021-08-29 00:00:49 +0900

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.scss';
+
+function Navbar(){
+  return (
+    <nav>
+      <div className='container'>
+        <div className='logo'>
+          <h1>♦️ WEBSITE NAME</h1>
+        </div>
+        <div className='buttons'>
+          <ul>
+            <li><a href=''>Home</a></li>
+            <li><a href=''>About</a></li>
+            <li><a href=''>Services</a></li>
+            <li><a href=''>Blog</a></li>
+            <li><a href=''>Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
