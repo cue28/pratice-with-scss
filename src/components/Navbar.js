f233a4ff9217c9c212da6a642bc41bb2d9d4100b
siyoon <younyaho@gmail.com>
2021-09-04 22:29:29 +0900

    feat:make navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import './Navbar.scss';
+import '../scss/Navbar.scss';
 
-function Navbar(){
+function Navbar() {
   return (
-    <nav>
-      {/* navbar */}
-      <div className='container'>
-        <div className='logo'>
+    <div className='nav__container'>
+      <nav className='navbar'>
+        <div className='navbar__logo'>
           <h1>♦️ WEBSITE NAME</h1>
         </div>
-        <div className='buttons'>
-          <ul>
-            <li><a href=''>Home</a></li>
-            <li><a href=''>About</a></li>
-            <li><a href=''>Services</a></li>
-            <li><a href=''>Blog</a></li>
-            <li><a href=''>Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+
+        <ul className='navbar__buttons'>
+          <li>
+            <a href=''>Home</a>
+          </li>
+          <li>
+            <a href=''>About</a>
+          </li>
+          <li>
+            <a href=''>Services</a>
+          </li>
+          <li>
+            <a href=''>Blog</a>
+          </li>
+          <li>
+            <a href=''>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
-};
+}
 
 export default Navbar;
