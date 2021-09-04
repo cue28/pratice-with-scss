e2cb6682b97b91f4a5084856907f6e1b46d7d550
siyoon <jeoncue@gmail.com>
2021-09-05 00:30:18 +0900

    is it works?

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -66,3 +66,14 @@ function Introduction() {
 }
 
 export default Introduction;
+
+// * 2e6b9ad = siyoon , 5 minutes ago : feat:add introduction part
+// * 43c9195 = siyoon , 85 minutes ago : feat:complete navbar and also media screen
+// * f233a4f = siyoon , 2 hours ago : feat:make navbar
+// *   5765e49 = siyoon , 7 days ago : Merge branch 'main' of https://github.com/cue28/pratice-with-scss
+// |\
+// | * 486fbd4 = Siyoon Jeon , 7 days ago : Update README.md
+// * | c6bd22e = siyoon , 7 days ago : feat:component of navbar
+// |/
+// * 79c277c = siyoon , 7 days ago : first commit
+// * 84049ed = siyoon , 7 days ago : Initialize project using Create React App
