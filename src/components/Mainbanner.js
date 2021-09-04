2e6b9ad5ff10447d669dee0a4e24afa45ceacaf0
siyoon <younyaho@gmail.com>
2021-09-05 00:04:02 +0900

    feat:add introduction part

---
+++ src/components/Mainbanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../scss/Mainbanner.scss';
+
+function Mainbanner(){
+  return (
+    <section className='Mainbanner'>
+    </section>
+  );
+};
+
+export default Mainbanner;
