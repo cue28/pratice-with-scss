43c9195f81113f9ca02fee9f1a85edbb4b21c97c
siyoon <younyaho@gmail.com>
2021-09-04 22:44:26 +0900

    feat:complete navbar and also media screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../scss/Navbar.scss';
 
@@ -26,6 +28,7 @@ function Navbar() {
             <a href=''>Contact</a>
           </li>
         </ul>
+          <FontAwesomeIcon icon={faBars} className='hambuger' />
       </nav>
     </div>
   );
